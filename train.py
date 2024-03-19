3b43429c93ca54caf5a50cdc01c9f94a20477bba
Irfan-Hamid <146963550+Irfan-Hamid@users.noreply.github.com>
2024-03-19 19:18:18 +0000

    work

--- train.py
+++ train.py
@@ -395,7 +395,7 @@ def train_model(config):
 
             global_step += 1
         
-        if epoch == 50:
+        if epoch == 49:
         # Run validation at the end of every epoch
             run_validation(model, val_dataloader, tokenizer_src, tokenizer_tgt, config['seq_len'], device, lambda msg: batch_iterator.write(msg), global_step, writer)
         # Save the model at the end of every epoch
