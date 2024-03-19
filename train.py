eef4542a9125c9cb5c8c0f017fc6e64d8e0e62f6
Irfan-Hamid <146963550+Irfan-Hamid@users.noreply.github.com>
2024-03-19 14:38:36 +0000

    19_(rj)

--- train.py
+++ train.py
@@ -143,7 +143,6 @@ def run_validation(model, validation_ds, tokenizer_src, tokenizer_tgt, max_len, 
 
     with torch.no_grad():
         for batch in validation_ds:
-            count += 1
             encoder_input = batch["encoder_input"].to(device) # (b, seq_len)
             encoder_mask = batch["encoder_mask"].to(device) # (b, 1, 1, seq_len)
 
