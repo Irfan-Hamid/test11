741dbbf75fad856ea3a3df6a39511cfde01a7d23
Irfan Hamid <irfanhamid19@gmail.com>
2024-03-20 01:45:40 +0000

    rj

--- dataset.py
+++ dataset.py
@@ -86,5 +86,5 @@ class BilingualDataset(Dataset):
         }
     
 def causal_mask(size):
-    mask = torch.triu(torch.ones((1, size, size)), diagonal=0).type(torch.int)
+    mask = torch.triu(torch.ones((1, size, size)), diagonal=1).type(torch.int)
     return mask == 0
